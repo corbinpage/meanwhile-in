46e7292a48a0f76c155ed70cf95bf7d6fa71e68d
Corbin Page <corbin.page@gmail.com>
2014-02-21 17:57:41 -0500

    Added search model

--- app/controllers/meanwhile_controller.rb
+++ app/controllers/meanwhile_controller.rb
@@ -3,22 +3,15 @@ class MeanwhileController < ApplicationController
   def index
     first = params[:text].nil? 
 
-    # text = !first  ? params[:text] : "Charlotte, NC"
-
     search = Search.new(start_time: Time.now)
-    #search = search.new(text: text)
     search.get_result
     search.save
 
     if !first
       respond_to do |format|
         if search.save
-          #format.html { redirect_to "meanwhile#index", notice: 'Search successfully.' }
-          #format.js   {"$('#pic').attr('src', 'instagram_example2.jpg');"}
-          #format.js   {alert("Here!");}
           format.json { render json: search}
         else
-          # format.html { redirect_to "meanwhile#index" }
           format.json { render json: search}
         end
       end
--- app/models/search.rb
+++ app/models/search.rb
@@ -1,23 +1,19 @@
 class Search < ActiveRecord::Base
-
+  CLIENT_ID = '5acd491087a349acbcbddde9c92ab5c1' # This is app specifc
+  CLIENT_SECRET = 'b17108a913f246bb97e6ddc62aba5f76'
+  
   #Creating an initialize method on the model doesn't work for some reason?
   #def initialize(text)
   #  @text = text
   #end
 
   def get_result
-    perform_search
-  end
 
-  def perform_search
-    #require 'open-uri'
-    #require 'net/http'
-
-    client_id = '5acd491087a349acbcbddde9c92ab5c1'
-    client_secret = 'b17108a913f246bb97e6ddc62aba5f76'
+    client_id = CLIENT_ID
+    client_secret = CLIENT_SECRET
 
     locations = {
-                  0 =>[40.7143528,74.0059731,"New York"],          # New York
+                  0 =>[40.7143528,74.0059731,"New York"],             # New York
                   1 =>[48.856614,2.3522219000000177,"Paris"],         # Paris
                   2 =>[43.585278,39.72027800000001,"Sochi"],          # Sochi
                   3 =>[34.0522342,118.2436849,"Los Angelos"],         # LA
